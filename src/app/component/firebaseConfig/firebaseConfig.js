692e02b17b266b629f4206b11f68e8714164664e
Shafinul Pasha <jishan523@gmail.com>
2024-01-30 13:13:24 +0600

    Configuring the firebase config file. 1. Functional component for firebase config 2. Exported the function 3. Connecting the database on return using getDatabase()

--- src/app/component/firebaseConfig/firebaseConfig.js
+++ src/app/component/firebaseConfig/firebaseConfig.js
@@ -1,23 +1,32 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getDatabase} from "firebase/database";
+import {getDatabase} from "firebase/databa
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDS2ja_YAMbc0m19p5DJ9KKIlZt3cNBfTs",
-  authDomain: "fir-starter-9f446.firebaseapp.com",
-  databaseURL: "https://fir-starter-9f446-default-rtdb.firebaseio.com",
-  projectId: "fir-starter-9f446",
-  storageBucket: "fir-starter-9f446.appspot.com",
-  messagingSenderId: "829420054204",
-  appId: "1:829420054204:web:ca8c474f05abede8bc92b9",
-  measurementId: "G-3XCKHXM9YP"
+
+// 1. Functional component for firebase config
+function FirebaseConfig(){
+    const firebaseConfig = {
+        apiKey: "AIzaSyDS2ja_YAMbc0m19p5DJ9KKIlZt3cNBfTs",
+        authDomain: "fir-starter-9f446.firebaseapp.com",
+        databaseURL: "https://fir-starter-9f446-default-rtdb.firebaseio.com",
+        projectId: "fir-starter-9f446",
+        storageBucket: "fir-starter-9f446.appspot.com",
+        messagingSenderId: "829420054204",
+        appId: "1:829420054204:web:ca8c474f05abede8bc92b9",
+        measurementId: "G-3XCKHXM9YP"
+      };
+      
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+      const analytics = getAnalytics(app);
+//   3. Connecting the database on return
+      return getDatabase(app);
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// 2. Exported the function
+export default FirebaseConfig;
